562bc5c5e1d8f4bf7757740514c3e523fa5eb84e
davetorrente <dave.torrente@gmail.com>
2017-11-02 21:32:20 -0700

    fix mlab

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url' : 'mongodb://dretorrente:Davepogi0#@ds243325.mlab.com:43325/kpop-project'
+    'url' : 'mongodb://davetorrente:Davepogi0#@ds243325.mlab.com:43325/kpop-project'
     // 'url' : 'mongodb://127.0.0.1:27017/kpop'
 
 };
